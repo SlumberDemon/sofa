0e94ceea739557fae1999b63d921c9739aa17c69
SlumberDemon <nextil3o3@gmail.com>
2023-10-19 19:23:34 +0000

    New code - thanks lemon

--- .sofa/sofa.py
+++ .sofa/sofa.py
@@ -1,163 +1,83 @@
-import sys
 import json
+import sys
 from pathlib import Path
-from material_color_utilities_python import *
 
-with open(str(Path.home()) + "/.sofa/config.json") as c:
-    config = json.load(c)
+from material_color_utilities_python import hexFromArgb, themeFromImage
+from PIL import Image
 
-if sys.argv[1] == "templater":
+config = json.loads(
+    Path.home().joinpath(".sofa", "config.json").read_text(encoding="utf-8")
+)
+placeholders = (
+    "primary",
+    "onPrimary",
+    "primaryContainer",
+    "onPrimaryContainer",
+    "secondary",
+    "onSecondary",
+    "secondaryContainer",
+    "onSecondaryContainer",
+    "tertiary",
+    "onTertiary",
+    "tertiaryContainer",
+    "onTertiaryContainer",
+    "error",
+    "onError",
+    "errorContainer",
+    "onErrorContainer",
+    "background",
+    "onBackground",
+    "surface",
+    "onSurface",
+    "surfaceVariant",
+    "onSurfaceVariant",
+    "outline",
+    "shadow",
+    "inverseSurface",
+    "inverseOnSurface",
+    "inversePrimary",
+)
+func_map = {
+    "{" + placeholder + "}": f"get_{placeholder}" for placeholder in placeholders
+}
 
-    def fetch_colors(type: str):  # light | dark
-        img = Image.open(str(Path.home()) + f"/{config['wallpath']}/wallpaper")
-        basewidth = 64
-        wpercent = basewidth / float(img.size[0])
-        hsize = int((float(img.size[1]) * float(wpercent)))
-        img = img.resize((basewidth, hsize), Image.Resampling.LANCZOS)
-        newtheme = themeFromImage(img)
 
-        colorscheme = newtheme.get("schemes").get(type)
+def fetch_colors(type: str):  # light | dark
+    img = Image.open(Path.home().joinpath(config["wallpath"], "wallpaper"))
+    basewidth = 64
+    wpercent = basewidth / float(img.size[0])
+    hsize = int(float(img.size[1]) * float(wpercent))
+    img = img.resize((basewidth, hsize), Image.Resampling.LANCZOS)
+    newtheme = themeFromImage(img)
+    return newtheme.get("schemes", {}).get(type)
 
-        return colorscheme
 
-    def type_converter(value, type: str):  # hex | argb
-        if type == "hex":
-            return str(hexFromArgb(value))
-        if type == "argb":
-            return str(value)
+def type_converter(value, type: str) -> str:  # hex | argb
+    if type == "hex":
+        return hexFromArgb(value)
+    return str(value)
 
-    def templater(colors):
-        for i in config["templates"]:
-            with open(str(Path.home()) + f"/.sofa/templates/{i['name']}", "rt") as a:
-                with open(str(Path.home()) + i["path"], "wt") as b:
-                    for l in a:
-                        b.write(
-                            l.replace(
-                                "{primary}",
-                                type_converter(colors.get_primary(), i["type"]),
-                            )
-                            .replace(
-                                "{onPrimary}",
-                                type_converter(colors.get_onPrimary(), i["type"]),
-                            )
-                            .replace(
-                                "{primaryContainer}",
-                                type_converter(
-                                    colors.get_primaryContainer(), i["type"]
-                                ),
-                            )
-                            .replace(
-                                "{onPrimaryContainer}",
-                                type_converter(
-                                    colors.get_onPrimaryContainer(), i["type"]
-                                ),
-                            )
-                            .replace(
-                                "{secondary}",
-                                type_converter(colors.get_secondary(), i["type"]),
-                            )
-                            .replace(
-                                "{onSecondary}",
-                                type_converter(colors.get_onSecondary(), i["type"]),
-                            )
-                            .replace(
-                                "{secondaryContainer}",
-                                type_converter(
-                                    colors.get_secondaryContainer(), i["type"]
-                                ),
-                            )
-                            .replace(
-                                "{onSecondaryContainer}",
-                                type_converter(
-                                    colors.get_onSecondaryContainer(), i["type"]
-                                ),
-                            )
-                            .replace(
-                                "{tertiary}",
-                                type_converter(colors.get_tertiary(), i["type"]),
-                            )
-                            .replace(
-                                "{onTertiary}",
-                                type_converter(colors.get_onTertiary(), i["type"]),
-                            )
-                            .replace(
-                                "{tertiaryContainer}",
-                                type_converter(
-                                    colors.get_tertiaryContainer(), i["type"]
-                                ),
-                            )
-                            .replace(
-                                "{onTertiaryContainer}",
-                                type_converter(
-                                    colors.get_onTertiaryContainer(), i["type"]
-                                ),
-                            )
-                            .replace(
-                                "{error}",
-                                type_converter(colors.get_error(), i["type"]),
-                            )
-                            .replace(
-                                "{onError}",
-                                type_converter(colors.get_onError(), i["type"]),
-                            )
-                            .replace(
-                                "{errorContainer}",
-                                type_converter(colors.get_errorContainer(), i["type"]),
-                            )
-                            .replace(
-                                "{onErrorContainer}",
-                                type_converter(
-                                    colors.get_onErrorContainer(), i["type"]
-                                ),
-                            )
-                            .replace(
-                                "{background}",
-                                type_converter(colors.get_background(), i["type"]),
-                            )
-                            .replace(
-                                "{onBackground}",
-                                type_converter(colors.get_onBackground(), i["type"]),
-                            )
-                            .replace(
-                                "{surface}",
-                                type_converter(colors.get_surface(), i["type"]),
-                            )
-                            .replace(
-                                "{onSurface}",
-                                type_converter(colors.get_onSurface(), i["type"]),
-                            )
-                            .replace(
-                                "{surfaceVariant}",
-                                type_converter(colors.get_surfaceVariant(), i["type"]),
-                            )
-                            .replace(
-                                "{onSurfaceVariant}",
-                                type_converter(
-                                    colors.get_onSurfaceVariant(), i["type"]
-                                ),
-                            )
-                            .replace(
-                                "{outline}",
-                                type_converter(colors.get_outline(), i["type"]),
-                            )
-                            .replace(
-                                "{shadow}",
-                                type_converter(colors.get_shadow(), i["type"]),
-                            )
-                            .replace(
-                                "{inverseSurface}",
-                                type_converter(colors.get_inverseSurface(), i["type"]),
-                            )
-                            .replace(
-                                "{inverseOnSurface}",
-                                type_converter(
-                                    colors.get_inverseOnSurface(), i["type"]
-                                ),
-                            )
-                            .replace(
-                                "{inversePrimary}",
-                                type_converter(colors.get_inversePrimary(), i["type"]),
-                            )
-                        )
 
-    templater(fetch_colors(config["mode"]))
+def templater(colors) -> None:
+    for template in config["templates"]:
+        with (
+            Path.home()
+            .joinpath(".sofa", "templates", template["name"])
+            .open(mode="r", encoding="utf-8") as a,
+            Path.home()
+            .joinpath(template["path"])
+            .open(mode="w", encoding="utf-8") as b,
+        ):
+            for line in a:
+                filled_line = line
+                for placeholder, func in func_map.items():
+                    filled_line = filled_line.replace(
+                        placeholder,
+                        type_converter(getattr(colors, func)(), template["type"]),
+                    )
+                b.write(filled_line)
+
+
+if __name__ == "__main__":
+    if sys.argv[1] == "templater":
+        templater(fetch_colors(config["mode"]))
